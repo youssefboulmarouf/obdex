f7ebf21c76e5e41b56e0060c0c4af296343b8bd7
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2023-02-01 20:26:23 -0500

    some esthetics

--- contracts/OBDex.sol
+++ contracts/OBDex.sol
@@ -6,11 +6,11 @@ import '../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol';
 contract OBDex {
     using SafeMath for uint;
 
-    // Enums
+    // --- Enums ---
     enum ORDER_SIDE { BUY, SELL }
     enum ORDER_TYPE { MARKET, LIMIT }
 
-    // Structs
+    // --- Structs ---
     struct Balance { 
         uint free; // Free balance
         uint locked; // Locked balance for orders in the order book
@@ -46,21 +46,21 @@ contract OBDex {
         uint date
     );
 
-    // Variables
-    address public admin; // Contract owner
-    bytes32[] public tickerList; 
-    mapping(bytes32 => Token) public tokens;
-    mapping(address => mapping(bytes32 => Balance)) public balances;
-    mapping(bytes32 => mapping(uint => Order[])) public orderBook;
-    uint public nextOrderId; // Order id tracker
-    uint public nextTradeId; // Trader id tracker
+    // --- Variables ---
+    address     public admin; // Contract owner
+    bytes32[]   public tickerList; 
+    uint        public nextOrderId; // Order id tracker
+    uint        public nextTradeId; // Trader id tracker
 
-
-    // Contract constructor
+    mapping (bytes32 => Token)                          public tokens;
+    mapping (address => mapping (bytes32 => Balance))   public balances;
+    mapping (bytes32 => mapping (uint => Order[]))      public orderBook;
+    
+    // --- Contract Constructor ---
     constructor() { admin = msg.sender; }
 
 
-    // Access Controle
+    // --- Access Controle ---
     modifier onlyAdmin() {
         require(admin == msg.sender, "Unauthorized! Only Admin can perform this action.");
         _;
