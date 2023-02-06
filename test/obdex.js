11a29fc03164983cb1050c67c02fb14ca0d45246
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2023-02-05 21:09:25 -0500

    add logic to seed trader accounts and get the balances

--- test/obdex.js
+++ test/obdex.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("OBDex", () => {
 
@@ -6,14 +7,41 @@ describe("OBDex", () => {
         // Contract Factory
         const contractFactory = await hre.ethers.getContractFactory(contractName);
         // Deploy Contract
-        const contract = await contractFactory.deploy({gasPrice: 50000000000});
+        const contract = await contractFactory.deploy();
         // Make sure the contracft is deployed
         await contract.deployed();
         
         return {contractFactory, contract};
     }
 
-    const fixture = async () => {
+    const seedTraderWallet = async (obdex, trader, tokens, amount) => {
+        await tokens.map(async token => {    
+            // Mint token for trader
+            await token.contract.faucet(trader.address, amount);
+            // Approve obdex to spend tokens
+            await token.contract.connect(trader).approve(obdex.contract.address, amount);
+            
+            // Deposit
+            //const tokenName = await token.contract.name();
+            //const ticker = ethers.utils.formatBytes32String(tokenName);
+            //await obdex.contract.connect(trader).deposit(ticker, amount);
+            // Balances
+            await getTraderBalance(obdex, token, trader);
+        });
+    }
+
+    const getTraderBalance = async(obdex, token, trader) => {
+        const tokenName = await token.contract.name();
+        const ticker = ethers.utils.formatBytes32String(tokenName);
+        const walletBalance = await trader.getBalance()
+        const dexBalances = await obdex.contract.balances(trader.address, ticker);
+        console.log("Trader Address:", trader.address);
+        console.log("ETH Balance:", walletBalance);
+        console.log("DEX Balance:", tokenName, dexBalances);
+        return {walletBalance, dexBalances};
+    }
+
+    const obdexFixture = async () => {
         // Deploy the contracts
         const [dai, bat, rep, zrx] = await Promise.all(
             ["Dai", "Bat", "Rep", "Zrx"].map(contractName => deploy(contractName))
@@ -29,9 +57,26 @@ describe("OBDex", () => {
             )
         )]);
 
+        const tokens = await obdex.contract.getTokens();
+        //console.log("tokens:", tokens);
+
         // Seed Traders Accounts
+        const amount = hre.ethers.utils.parseUnits('1000', 'ether');
         // --- hardhat accounts
         const [owner, trader1, trader2, trader3, trader4, others] = await ethers.getSigners();
+        await Promise.all([
+            [trader1, trader2, trader3, trader4].map(trader => 
+                seedTraderWallet(obdex, trader, [dai, bat, rep, zrx], amount)
+            )
+        ]);
     }
+
+    describe("TEST", () => {
+
+        it("1", async () => {
+            await loadFixture(obdexFixture);
+        });
+        
+    });
 
 });
