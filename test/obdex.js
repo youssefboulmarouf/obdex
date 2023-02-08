0977d666ac0b3ee995eb62cba6f96709c04d801c
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2023-02-07 21:15:08 -0500

    added balance test

--- test/obdex.js
+++ test/obdex.js
@@ -3,6 +3,8 @@ const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 
 describe('OBDex', () => {
 
+    const [DAI, REP, BAT, ZRX] = ['DAI', 'REP', 'BAT', 'ZRX'].map(token => hre.ethers.utils.formatBytes32String(token));
+
     const deploy = async (contractName) => {
         // Contract Factory
         const contractFactory = await hre.ethers.getContractFactory(contractName);
@@ -27,7 +29,7 @@ describe('OBDex', () => {
 
     const getTraderBalance = async(obdex, token, trader) => {
         const tokenName = await token.contract.name();
-        const ticker = ethers.utils.formatBytes32String(tokenName);
+        const ticker = hre.ethers.utils.formatBytes32String(tokenName);
 
         // Trader's balance of Token in OBDex Wallet 
         const obdexBalances = await obdex.contract.balances(trader.address, ticker);
@@ -78,25 +80,18 @@ describe('OBDex', () => {
             await loadFixture(tokenFixture);
 
             await expect(
-                obdex.contract.connect(trader).addToken(
-                    hre.ethers.utils.formatBytes32String('DAI'),
-                    dai.contract.address
-                )
+                obdex.contract.connect(trader).addToken(DAI, dai.contract.address)
             ).to.be.revertedWith('Unauthorized! Only Admin can perform this action.');
         });
 
         it('Should Have Correct Tokens', async () => {
             await loadFixture(tokenFixture);
 
-            await obdex.contract.connect(owner).addToken(
-                hre.ethers.utils.formatBytes32String('DAI'),
-                dai.contract.address
-            );
-
-            const tokens = await obdex.contract.connect(owner).getTokens();
+            await obdex.contract.connect(owner).addToken(DAI, dai.contract.address);
             
+            const tokens = await obdex.contract.connect(owner).getTokens();
             expect(tokens.length).to.be.equals(1);
-            expect(tokens[0].ticker).to.be.equals(hre.ethers.utils.formatBytes32String('DAI'));
+            expect(tokens[0].ticker).to.be.equals(DAI);
             expect(tokens[0].tokenAddress).to.be.equals(dai.contract.address);
 
         });
@@ -104,31 +99,21 @@ describe('OBDex', () => {
         it('Should NOT Add Token Twice', async () => {
             await loadFixture(tokenFixture);
 
-            await obdex.contract.connect(owner).addToken(
-                hre.ethers.utils.formatBytes32String('DAI'),
-                dai.contract.address
-            );
+            await obdex.contract.connect(owner).addToken(DAI, dai.contract.address);
 
             await expect(
-                obdex.contract.connect(owner).addToken(
-                    hre.ethers.utils.formatBytes32String('DAI'),
-                    dai.contract.address
-                )
+                obdex.contract.connect(owner).addToken(DAI, dai.contract.address)
             ).to.be.revertedWith('Ticker Already Exist!');
         });
 
         it('Should have correct Ticker list', async () => {
             await loadFixture(tokenFixture);
 
-            await obdex.contract.connect(owner).addToken(
-                hre.ethers.utils.formatBytes32String('DAI'),
-                dai.contract.address
-            );
+            await obdex.contract.connect(owner).addToken(DAI, dai.contract.address);
 
             const tickerList = await obdex.contract.connect(owner).getTickerList();
-            
             expect(tickerList.length).to.be.equals(1);
-            expect(tickerList[0]).to.be.equals(hre.ethers.utils.formatBytes32String('DAI'));
+            expect(tickerList[0]).to.be.equals(DAI);
         });
         
     });
@@ -142,10 +127,7 @@ describe('OBDex', () => {
 
             [owner, trader] = await hre.ethers.getSigners();
 
-            await obdex.contract.connect(owner).addToken(
-                hre.ethers.utils.formatBytes32String('DAI'),
-                dai.contract.address
-            );
+            await obdex.contract.connect(owner).addToken(DAI, dai.contract.address);
         }
 
         it('Should NOT Deposit If Ticker Does NOT Exist', async () => {
@@ -176,10 +158,7 @@ describe('OBDex', () => {
             //await seedTraderWallet(obdex, trader, [dai], amount);
 
             // Deposit
-            await obdex.contract.connect(trader).deposit(
-                ethers.utils.formatBytes32String('DAI'), 
-                amount
-            );
+            await obdex.contract.connect(trader).deposit(DAI, amount);
 
             // Balances
             const obdexBalances = await getTraderBalance(obdex, dai, trader);
@@ -200,19 +179,13 @@ describe('OBDex', () => {
 
             [owner, trader] = await hre.ethers.getSigners();
 
-            await obdex.contract.connect(owner).addToken(
-                hre.ethers.utils.formatBytes32String('DAI'),
-                dai.contract.address
-            );
+            await obdex.contract.connect(owner).addToken(DAI, dai.contract.address);
             
             amount = hre.ethers.utils.parseUnits('1000', 'ether');
             
             await seedTraderWallet(obdex, trader, [dai], amount);
             
-            await obdex.contract.connect(trader).deposit(
-                ethers.utils.formatBytes32String('DAI'), 
-                amount
-            );
+            await obdex.contract.connect(trader).deposit(DAI, amount);
         }
 
         it('Should withdraw if enough Balance', async() => {
@@ -224,10 +197,7 @@ describe('OBDex', () => {
             expect(daiBalance).to.be.equals(0);
 
             const withdrawAmount = hre.ethers.utils.parseUnits('100', 'ether');
-            await obdex.contract.connect(trader).withdraw(
-                hre.ethers.utils.formatBytes32String('DAI'),
-                withdrawAmount
-            );
+            await obdex.contract.connect(trader).withdraw(DAI, withdrawAmount);
 
             obdexBalances = await getTraderBalance(obdex, dai, trader);
             daiBalance = await dai.contract.balanceOf(trader.address);
@@ -242,10 +212,7 @@ describe('OBDex', () => {
 
             const withdrawAmount = hre.ethers.utils.parseUnits('2000', 'ether');
             await expect(
-                obdex.contract.connect(trader).withdraw(
-                    hre.ethers.utils.formatBytes32String('DAI'),
-                    withdrawAmount
-                )
+                obdex.contract.connect(trader).withdraw(DAI, withdrawAmount)
             ).to.be.revertedWith('Low Token Balance!');
         });
         
@@ -262,7 +229,52 @@ describe('OBDex', () => {
         });
     });
 
-    describe('Balance', () => {});
+    describe('Balance', () => {
+        let obdex, dai, rep, owner, trader, amount;
+
+        const balanceFixture = async () => {
+            obdex = await deploy('OBDex');
+            dai = await deploy('Dai');
+            rep = await deploy('Rep');
+
+            [owner, trader1, trader2] = await hre.ethers.getSigners();
+
+            await obdex.contract.connect(owner).addToken(DAI, dai.contract.address);
+            await obdex.contract.connect(owner).addToken(REP, rep.contract.address);
+            
+            amount = hre.ethers.utils.parseUnits('1000', 'ether');
+            
+            await seedTraderWallet(obdex, trader1, [dai, rep], amount);
+            await obdex.contract.connect(trader1).deposit(DAI, amount);
+
+            await seedTraderWallet(obdex, trader2, [dai, rep], amount);
+            await obdex.contract.connect(trader2).deposit(REP, amount);
+        }
+
+        it('Should have correct Token Balances', async () => {
+            await loadFixture(balanceFixture);
+            
+            // Trader 1 Balances
+            let obdexDaiBalances = await getTraderBalance(obdex, dai, trader1);
+            let obdexRepBalances = await getTraderBalance(obdex, rep, trader1);
+            let daiBalance = await dai.contract.balanceOf(trader1.address);
+            let repBalance = await rep.contract.balanceOf(trader1.address);
+            expect(obdexDaiBalances.free).to.be.equals(amount);
+            expect(obdexRepBalances.free).to.be.equals(0);
+            expect(daiBalance).to.be.equals(0);
+            expect(repBalance).to.be.equals(amount);
+
+            // Trader 2 Balances
+            obdexDaiBalances = await getTraderBalance(obdex, dai, trader2);
+            obdexRepBalances = await getTraderBalance(obdex, rep, trader2);
+            daiBalance = await dai.contract.balanceOf(trader2.address);
+            repBalance = await rep.contract.balanceOf(trader2.address);
+            expect(obdexDaiBalances.free).to.be.equals(0);
+            expect(obdexRepBalances.free).to.be.equals(amount);
+            expect(daiBalance).to.be.equals(amount);
+            expect(repBalance).to.be.equals(0);
+        });
+    });
 
     describe('Limit Order', () => {});
 
