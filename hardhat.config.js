b053d1da96ed27f9e40d116f27c5fa43245c21a1
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2023-02-12 00:16:30 -0500

    adjust hardhat config to support large contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,25 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true
+    }
+  },
   paths: {
     artifacts: 'client/src/artifacts' // Built contracts location
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      }
+    ]
   }
 };
