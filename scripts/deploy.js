2d281b9f044b2aa527b033b48ca9957569460721
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2023-02-19 14:15:54 -0500

    deploy script to to convert amount to ether unit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,7 +9,11 @@ const helpers = require("@nomicfoundation/hardhat-network-helpers");
 const ORDER_SIDE = {BUY: 0, SELL: 1};
 const ORDER_TYPE = {MARKET: 0, LIMIT: 1};
 
-const amount = hre.ethers.utils.parseUnits('1000', 'ether');
+const toEthUnit = (amount) => {
+    return hre.ethers.utils.parseUnits(amount.toString(), 'ether');
+}
+
+const amount = toEthUnit(20000);
 const [DAI, BAT, REP, ZRX] = ["DAI", "BAT", "REP", "ZRX"].map(tokenName => hre.ethers.utils.formatBytes32String(tokenName));
 
 const deploy = async (contractName) => {
@@ -22,102 +26,103 @@ const deploy = async (contractName) => {
     return {contractFactory, contract};
 }
 
-const seedTraderAccount = async (dex, tokens, amount, trader) => {
+const seedTraderAccount = async (obdex, tokens, amount, trader) => {
     await tokens.map(async token => {
         // Mint token for trader
         await token.contract.faucet(trader.address, amount);
         // Approve dex to spend tokens
-        await token.contract.connect(trader).approve(dex.contract.address, amount);
+        await token.contract.connect(trader).approve(obdex.contract.address, amount);
         
         const tokenName = await token.contract.name();
         const ticker = ethers.utils.formatBytes32String(tokenName);
         // Deposit
-        await dex.contract.connect(trader).deposit(ticker, amount);
+        const amountToDeposit = toEthUnit(19000);
+        await obdex.contract.connect(trader).deposit(ticker, amountToDeposit);
         // Balances
-        await getTraderBalance(dex, token, trader);
+        //await getTraderBalance(obdex, token, trader);
     });
 }
 
-const getTraderBalance = async(dex, token, trader) => {
+const getTraderBalance = async(obdex, token, trader) => {
     const tokenName = await token.contract.name();
     const ticker = ethers.utils.formatBytes32String(tokenName);
     const walletBalance = await trader.getBalance();
     const tokenBalance = await token.contract.balanceOf(trader.address);
-    const dexBalances = await dex.contract.balances(trader.address, ticker);
+    const dexBalances = await obdex.contract.balances(trader.address, ticker);
     console.log("Trader Address:", trader.address);
-    console.log("ETH Balance:", walletBalance);
-    console.log("TOKEN Balance:", tokenBalance);
+    //console.log("ETH Balance:", walletBalance);
+    //console.log("TOKEN Balance:", tokenBalance);
     console.log("DEX Balance:", tokenName, dexBalances);
     return {walletBalance, dexBalances};
 }
 
-const createOrders = async (dex, traders) => {
-    await dex.contract.connect(traders[0]).createLimitOrder(BAT, 10, 10, ORDER_SIDE.BUY);
-    await dex.contract.connect(traders[1]).createMarketOrder(BAT, 10, ORDER_SIDE.SELL);
+const createOrders = async (obdex, traders) => {
+    await obdex.contract.connect(traders[0]).createLimitOrder(BAT, toEthUnit(10), 10, ORDER_SIDE.BUY);
+    await obdex.contract.connect(traders[1]).createMarketOrder(BAT, toEthUnit(10), ORDER_SIDE.SELL);
     await helpers.time.increase(1);
-    await dex.contract.connect(traders[0]).createLimitOrder(REP, 12, 11, ORDER_SIDE.BUY);
-    await dex.contract.connect(traders[1]).createMarketOrder(REP, 12, ORDER_SIDE.SELL);
+    await obdex.contract.connect(traders[0]).createLimitOrder(REP, toEthUnit(12), 11, ORDER_SIDE.BUY);
+    await obdex.contract.connect(traders[1]).createMarketOrder(REP, toEthUnit(12), ORDER_SIDE.SELL);
     await helpers.time.increase(1);
-    await dex.contract.connect(traders[0]).createLimitOrder(ZRX, 12, 15, ORDER_SIDE.BUY);
-    await dex.contract.connect(traders[1]).createMarketOrder(ZRX, 12, ORDER_SIDE.SELL);
+    await obdex.contract.connect(traders[0]).createLimitOrder(ZRX, toEthUnit(12), 15, ORDER_SIDE.BUY);
+    await obdex.contract.connect(traders[1]).createMarketOrder(ZRX, toEthUnit(12), ORDER_SIDE.SELL);
     await helpers.time.increase(1);
-    await dex.contract.connect(traders[0]).createLimitOrder(BAT, 15, 14, ORDER_SIDE.BUY);
-    await dex.contract.connect(traders[1]).createMarketOrder(BAT, 15, ORDER_SIDE.SELL);
+    await obdex.contract.connect(traders[0]).createLimitOrder(BAT, toEthUnit(15), 14, ORDER_SIDE.BUY);
+    await obdex.contract.connect(traders[1]).createMarketOrder(BAT, toEthUnit(15), ORDER_SIDE.SELL);
     await helpers.time.increase(1);
-    await dex.contract.connect(traders[0]).createLimitOrder(ZRX, 20, 12, ORDER_SIDE.BUY);
-    await dex.contract.connect(traders[1]).createMarketOrder(ZRX, 20, ORDER_SIDE.SELL);
+    await obdex.contract.connect(traders[0]).createLimitOrder(ZRX, toEthUnit(20), 12, ORDER_SIDE.BUY);
+    await obdex.contract.connect(traders[1]).createMarketOrder(ZRX, toEthUnit(20), ORDER_SIDE.SELL);
 
-    await dex.contract.connect(traders[0]).createLimitOrder(REP, 10, 2, ORDER_SIDE.BUY);
-    await dex.contract.connect(traders[1]).createMarketOrder(REP, 10, ORDER_SIDE.SELL);
+    await obdex.contract.connect(traders[0]).createLimitOrder(REP, toEthUnit(10), 2, ORDER_SIDE.BUY);
+    await obdex.contract.connect(traders[1]).createMarketOrder(REP, toEthUnit(10), ORDER_SIDE.SELL);
     await helpers.time.increase(1);
-    await dex.contract.connect(traders[0]).createLimitOrder(BAT, 5, 4, ORDER_SIDE.BUY);
-    await dex.contract.connect(traders[1]).createMarketOrder(BAT, 5, ORDER_SIDE.SELL);
+    await obdex.contract.connect(traders[0]).createLimitOrder(BAT, toEthUnit(5), 4, ORDER_SIDE.BUY);
+    await obdex.contract.connect(traders[1]).createMarketOrder(BAT, toEthUnit(5), ORDER_SIDE.SELL);
     await helpers.time.increase(1);
-    await dex.contract.connect(traders[0]).createLimitOrder(ZRX, 8, 2, ORDER_SIDE.BUY);
-    await dex.contract.connect(traders[1]).createMarketOrder(ZRX, 8, ORDER_SIDE.SELL);
+    await obdex.contract.connect(traders[0]).createLimitOrder(ZRX, toEthUnit(8), 2, ORDER_SIDE.BUY);
+    await obdex.contract.connect(traders[1]).createMarketOrder(ZRX, toEthUnit(8), ORDER_SIDE.SELL);
     await helpers.time.increase(1);
-    await dex.contract.connect(traders[0]).createLimitOrder(REP, 12, 6, ORDER_SIDE.BUY);
-    await dex.contract.connect(traders[1]).createMarketOrder(REP, 12, ORDER_SIDE.SELL);
+    await obdex.contract.connect(traders[0]).createLimitOrder(REP, toEthUnit(12), 6, ORDER_SIDE.BUY);
+    await obdex.contract.connect(traders[1]).createMarketOrder(REP, toEthUnit(12), ORDER_SIDE.SELL);
 
-    await dex.contract.connect(traders[0]).createLimitOrder(BAT, 10, 10, ORDER_SIDE.BUY);
-    await dex.contract.connect(traders[1]).createLimitOrder(BAT, 5, 10, ORDER_SIDE.SELL);
+    await obdex.contract.connect(traders[0]).createLimitOrder(BAT, toEthUnit(10), 10, ORDER_SIDE.BUY);
+    await obdex.contract.connect(traders[1]).createLimitOrder(BAT, toEthUnit(5), 10, ORDER_SIDE.SELL);
     
     await helpers.time.increase(1);
 
-    await dex.contract.connect(traders[1]).createLimitOrder(BAT, 5, 10, ORDER_SIDE.SELL);
+    await obdex.contract.connect(traders[1]).createLimitOrder(BAT, toEthUnit(5), 10, ORDER_SIDE.SELL);
     await helpers.time.increase(1);
 
     await Promise.all([
-        dex.contract.connect(traders[0]).createLimitOrder(BAT, 14, 10, ORDER_SIDE.BUY),
-        dex.contract.connect(traders[1]).createLimitOrder(BAT, 12, 11, ORDER_SIDE.BUY),
-        dex.contract.connect(traders[1]).createLimitOrder(BAT, 10, 12, ORDER_SIDE.BUY),
+        obdex.contract.connect(traders[0]).createLimitOrder(BAT, toEthUnit(14), 10, ORDER_SIDE.BUY),
+        obdex.contract.connect(traders[1]).createLimitOrder(BAT, toEthUnit(12), 11, ORDER_SIDE.BUY),
+        obdex.contract.connect(traders[1]).createLimitOrder(BAT, toEthUnit(10), 12, ORDER_SIDE.BUY),
 
-        dex.contract.connect(traders[0]).createLimitOrder(REP, 30, 4, ORDER_SIDE.BUY),
-        dex.contract.connect(traders[0]).createLimitOrder(REP, 20, 5, ORDER_SIDE.BUY),
-        dex.contract.connect(traders[1]).createLimitOrder(REP, 5, 6, ORDER_SIDE.BUY),
+        obdex.contract.connect(traders[0]).createLimitOrder(REP, toEthUnit(30), 4, ORDER_SIDE.BUY),
+        obdex.contract.connect(traders[0]).createLimitOrder(REP, toEthUnit(20), 5, ORDER_SIDE.BUY),
+        obdex.contract.connect(traders[1]).createLimitOrder(REP, toEthUnit(5), 6, ORDER_SIDE.BUY),
 
-        dex.contract.connect(traders[0]).createLimitOrder(ZRX, 40, 12, ORDER_SIDE.BUY),
-        dex.contract.connect(traders[0]).createLimitOrder(ZRX, 30, 13, ORDER_SIDE.BUY),
-        dex.contract.connect(traders[1]).createLimitOrder(ZRX, 5, 14, ORDER_SIDE.BUY),
+        obdex.contract.connect(traders[0]).createLimitOrder(ZRX, toEthUnit(40), 12, ORDER_SIDE.BUY),
+        obdex.contract.connect(traders[0]).createLimitOrder(ZRX, toEthUnit(30), 13, ORDER_SIDE.BUY),
+        obdex.contract.connect(traders[1]).createLimitOrder(ZRX, toEthUnit(5), 14, ORDER_SIDE.BUY),
 
-        dex.contract.connect(traders[2]).createLimitOrder(BAT, 20, 16, ORDER_SIDE.SELL),
-        dex.contract.connect(traders[3]).createLimitOrder(BAT, 30, 15, ORDER_SIDE.SELL),
-        dex.contract.connect(traders[3]).createLimitOrder(BAT, 5, 14, ORDER_SIDE.SELL),
+        obdex.contract.connect(traders[2]).createLimitOrder(BAT, toEthUnit(20), 16, ORDER_SIDE.SELL),
+        obdex.contract.connect(traders[3]).createLimitOrder(BAT, toEthUnit(30), 15, ORDER_SIDE.SELL),
+        obdex.contract.connect(traders[3]).createLimitOrder(BAT, toEthUnit(5), 14, ORDER_SIDE.SELL),
 
-        dex.contract.connect(traders[2]).createLimitOrder(REP, 40, 10, ORDER_SIDE.SELL),
-        dex.contract.connect(traders[2]).createLimitOrder(REP, 20, 9, ORDER_SIDE.SELL),
-        dex.contract.connect(traders[3]).createLimitOrder(REP, 8, 8, ORDER_SIDE.SELL),
+        obdex.contract.connect(traders[2]).createLimitOrder(REP, toEthUnit(40), 10, ORDER_SIDE.SELL),
+        obdex.contract.connect(traders[2]).createLimitOrder(REP, toEthUnit(20), 9, ORDER_SIDE.SELL),
+        obdex.contract.connect(traders[3]).createLimitOrder(REP, toEthUnit(8), 8, ORDER_SIDE.SELL),
 
-        dex.contract.connect(traders[2]).createLimitOrder(ZRX, 15, 23, ORDER_SIDE.SELL),
-        dex.contract.connect(traders[2]).createLimitOrder(ZRX, 12, 22, ORDER_SIDE.SELL),
-        dex.contract.connect(traders[3]).createLimitOrder(ZRX, 9, 21, ORDER_SIDE.SELL)
+        obdex.contract.connect(traders[2]).createLimitOrder(ZRX, toEthUnit(15), 23, ORDER_SIDE.SELL),
+        obdex.contract.connect(traders[2]).createLimitOrder(ZRX, toEthUnit(12), 22, ORDER_SIDE.SELL),
+        obdex.contract.connect(traders[3]).createLimitOrder(ZRX, toEthUnit(9), 21, ORDER_SIDE.SELL)
     ]);
   
 }
 
 async function main() {
     // Deploying all the contracts
-    const [dex, dai, bat, rep, zrx] = await Promise.all(
+    const [obdex, dai, bat, rep, zrx] = await Promise.all(
       ["OBDex", "Dai", "Bat", "Rep", "Zrx"].map(contractName => deploy(contractName))
     );
       
@@ -127,33 +132,46 @@ async function main() {
 
     // Add tokens
     await Promise.all([
-      [[DAI, dai], [ZRX, zrx], [REP, rep], [BAT, bat]]
-        .map(([ticker, token]) => dex.contract.addToken(ticker, token.contract.address))
+        [[DAI, dai], [ZRX, zrx], [REP, rep], [BAT, bat]]
+            .map(([ticker, token]) => obdex.contract.addToken(ticker, token.contract.address))
     ]);
 
-    const tokens = await dex.contract.getTokens();
+    const tokens = await obdex.contract.getTokens();
     console.log("tokens:", tokens);
 
     await Promise.all([
-      [trader1, trader2, trader3, trader4].map(trader => seedTraderAccount(dex, [dai, bat, rep, zrx], amount, trader))
+        [trader1, trader2, trader3, trader4].map(trader => {
+            console.log('trader; ', trader.address);
+            seedTraderAccount(obdex, [dai, bat, rep, zrx], amount, trader);
+        })
     ]);
   
     setTimeout(async () => {
-        await createOrders(dex, [trader1, trader2, trader3, trader4])
+        await createOrders(obdex, [trader1, trader2, trader3, trader4])
 
         let Orders = await Promise.all([
-            dex.contract.getOrders(ZRX, ORDER_SIDE.BUY),
-            dex.contract.getOrders(ZRX, ORDER_SIDE.SELL),
+            obdex.contract.getOrders(ZRX, ORDER_SIDE.BUY),
+            obdex.contract.getOrders(ZRX, ORDER_SIDE.SELL),
         ]);
 
-        console.log("BUY: ", Orders[0]);
-        console.log("SELL: ", Orders[1]);
+        //console.log("BUY: ", Orders[0]);
+        //console.log("SELL: ", Orders[1]);
 
+        // console.log('OPENING ORDERS');
+        // console.log('BUYING AMOUNTBAT: ', toEthUnit(10));
+
+        // console.log('Balance before');
+        // await getTraderBalance(obdex, dai, trader1);
+        
+        // await obdex.contract.connect(trader1).createLimitOrder(BAT, toEthUnit(10), 1, ORDER_SIDE.BUY);
+
+        // console.log('Balance after');
+        // await getTraderBalance(obdex, dai, trader1);
     }, 500);
-
+    
 
     const adresses = {
-        OBDex: dex.contract.address,
+        OBDex: obdex.contract.address,
         dai: dai.contract.address,
         bat: bat.contract.address,
         rep: rep.contract.address,
@@ -167,6 +185,7 @@ async function main() {
         { flag: 'w' }
     );
     console.log(adresses);
+    
 }
 
 // We recommend this pattern to be able to use async/await everywhere
